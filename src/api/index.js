c7cadd2a68b004eb5f23b259212a984829683220
Neeraj Mehra <neerajmehra448@gmail.com>
2025-02-09 15:06:55 +0530

    Title: Add API's

---
+++ src/api/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+const router = express.Router();
+import upload from "../helper/common/multerConfig.js";
+import { validator } from "../helper/common/validator.js";
+import authMiddleware from "../helper/common/jwtMiddelware.js";
+import {
+    deleteUser,
+    getUserList,
+    userRegister,
+    getUserDetail,
+    updateUserDetail,
+    uploadFileOnLocal,
+    uploadProfileImage,
+} from "./controller.js";
+
+router.post("/getUserList", getUserList);
+router.delete("/deleteUser", authMiddleware, deleteUser);
+router.get("/getUserDetail", authMiddleware, getUserDetail);
+router.put("/updateUserDetail", authMiddleware, updateUserDetail);
+router.post("/uploadFileOnLocal", authMiddleware, uploadFileOnLocal);
+router.post("/userRegister", validator("registerValidation"), userRegister);
+router.post("/upload", authMiddleware, upload.single("profileImage"), uploadProfileImage);
+
+export default router;
